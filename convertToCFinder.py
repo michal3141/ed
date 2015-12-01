44ee0110adb726a39133a42129d2812d5daae5e9
Pszczolka, Mateusz <Mateusz.Pszczolka@sabre.com>
2015-12-01 11:59:30 +0100

    www

--- convertToCFinder.py
+++ convertToCFinder.py
@@ -7,9 +7,18 @@ import sys
 
 def convert(name):
     target = open("cFinder/" + name + ".nt", 'w')
+    lines = {}
     for line in open('images/' + name + '.gv'):
         if "--" in line or "->" in line:
-            target.write(" ".join([edge.strip() for edge in line.replace("\n", "").replace("->", "--").split("--")]) + "\n")
+            leftRight = tuple(
+                sorted([edge.strip() for edge in line.replace("\n", "").replace("->", "--").split("--")], key=str.lower)
+            )
+            if leftRight in lines:
+                lines[leftRight] += 1
+            else:
+                lines[leftRight] = 1
+
+    target.write("\n".join(["%s %s %s" % (leftRight[0], leftRight[1], float(weight))  for (leftRight, weight) in lines.items()]))
 
 
 if __name__ == '__main__':
