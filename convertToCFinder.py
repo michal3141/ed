2f9a25942587c2f4accaf002a8ef4293f6cf9df2
Pszczolka, Mateusz <Mateusz.Pszczolka@sabre.com>
2015-12-01 12:41:33 +0100

    imporove conertToCFiner script - add edge density

--- convertToCFinder.py
+++ convertToCFinder.py
@@ -6,19 +6,20 @@ import sys
 
 
 def convert(name):
-    target = open("cFinder/" + name + ".nt", 'w')
+    target = open("cFinder/" + name + "_gt1.nt", 'w')
     lines = {}
     for line in open('images/' + name + '.gv'):
         if "--" in line or "->" in line:
             leftRight = tuple(
                 sorted([edge.strip() for edge in line.replace("\n", "").replace("->", "--").split("--")], key=str.lower)
             )
+            leftRight = tuple([vertex.replace(" ", "_").replace("\"", "").replace("(", "").replace(")", "") for vertex in leftRight])
             if leftRight in lines:
                 lines[leftRight] += 1
             else:
                 lines[leftRight] = 1
 
-    target.write("\n".join(["%s %s %s" % (leftRight[0], leftRight[1], float(weight))  for (leftRight, weight) in lines.items()]))
+    target.write("\n".join(["%s %s %s" % (leftRight[0], leftRight[1], weight)  for (leftRight, weight) in lines.items() if weight > 1]))
 
 
 if __name__ == '__main__':
